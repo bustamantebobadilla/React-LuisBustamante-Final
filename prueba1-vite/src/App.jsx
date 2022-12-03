292a78a1beee755768dc10ce88d690d10f361e24
luis bustamante <bustamantebobadilla@gmail.com>
2022-12-03 16:12:16 -0600

    Agrego hook de conteos

--- prueba1-vite/src/App.jsx
+++ prueba1-vite/src/App.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar/NavBar';
 
 function App() {
-  
+
   return (
     <>
       <NavBar />
--- prueba1-vite/src/components/ItemListContainer/ItemListContainer/ItemListContainer.jsx
+++ prueba1-vite/src/components/ItemListContainer/ItemListContainer/ItemListContainer.jsx
@@ -2,9 +2,20 @@ import './ItemListContainer.css'
 import { Item } from "../../Item/Item"
 import { NavBar } from "../../NavBar/NavBar"
 import {Titulo} from "../../Titulo/Titulo"
+import { useState } from 'react';
+
+
 
 const ItemListContainer = (obj) => {
   let tituloApp = "YOU Languaje Method";
+
+  const [conteo, setCount] = useState(0)
+  const sumacontador = ()=>{
+    setCount(conteo + 1)
+  }
+  const restacontador = ()=>{
+    setCount(conteo - 1)
+  } 
   return (
     <div className="contenedor-items">
       <Titulo
@@ -13,6 +24,9 @@ const ItemListContainer = (obj) => {
       />
       <Item />
       <label>{obj.saludo}</label>
+      <p className='alert alert-danger'>{conteo}</p>
+      <button className='btn btn-outline-primary' onClick={restacontador}>-</button>
+      <button className='btn btn-outline-primary' onClick={sumacontador}>+</button>
     </div>
   );
 };
